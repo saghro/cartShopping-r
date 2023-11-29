2f32ab859e6f9cbfe50ff7aaa51d8438bf7400d5
saghro <ayoubsaghro27@gmail.com>
2023-11-29 18:28:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+    /*
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+
+  return (
+    <>
+
+      <button onClick={increment}>Incrémenter</button>
+    </>
+  );*/
+}
+
+export default App;
+
