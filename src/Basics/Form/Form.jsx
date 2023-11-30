e05c4adace88a6d6071f176073b7b958176bf219
saghro <ayoubsaghro27@gmail.com>
2023-12-01 00:12:47 +0100

    addcity

--- src/Basics/Form/Form.jsx
+++ src/Basics/Form/Form.jsx
@@ -3,33 +3,33 @@ import {useState} from "react";
 export default function Form(){
     const[inputName ,setInputName]=useState();
     const [inputAge ,setInputAge]=useState('');
+    const [inputCity , setInputCity]=useState('')
     const [inputCondition ,setInputCondition]=useState(false);
 
-    const getName = ()=>{
-        const nameValue = document.querySelector('#name').value;
-        console.log(nameValue)
-        setInputName(nameValue);
+    const handleChange = (e)=>{
+        console.log(e.currentTarget.value)
     }
-    const getAge = ()=>{
-        const ageValue = document.querySelector('#age').value;
-        console.log(ageValue)
-        setInputAge(ageValue);
-    }
+
     const getCondition = ()=>{
-        const conditionValue = document.querySelector("#accept").checked
-        console.log(conditionValue)
+        const conditionValue = document.querySelector("#accept").checked;
         setInputCondition(conditionValue);
     }
     return(
+
         <div className='container my-4'>
+
            <form>
                <div className="form-group">
                    <label>Name :</label>
-                   <input type="text"  id="name" className="form-control" onChange={getName}/>
+                   <input type="text"  id="name" className="form-control" onChange={handleChange}/>
                </div>
                <div className="form-group">
                    <label>Age :</label>
-                   <input type="text"  id="age" className="form-control" onChange={getAge}/>
+                   <input type="text"  id="age" className="form-control" onChange={handleChange}/>
+               </div>
+               <div className="form-group">
+                   <label>City :</label>
+                   <input type="text"  id="city" className="form-control" onChange={handleChange}/>
                </div>
                <div className="form-check">
                    <input type="checkbox"  id="accept" className="form-check-input" onClick={getCondition} />
