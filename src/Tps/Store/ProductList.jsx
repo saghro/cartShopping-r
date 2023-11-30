b531e584beab05249b000be429843cbd4eb7a97a
saghro <ayoubsaghro27@gmail.com>
2023-11-30 17:55:22 +0100

    add categor

--- src/Tps/Store/ProductList.jsx
+++ src/Tps/Store/ProductList.jsx
@@ -3,16 +3,23 @@ import Product from "./Product";
 
 export default function ProductList() {
     const [productList, setProductList] = useState([]);
-    const [searchInput , setSearchInput]=useState('')
-    const [categories , setCategories]=useState('')
+    const [searchInput, setSearchInput] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('');
 
     const displayProduct = () => {
-        const productTemp = productList.filter(product => {
+        let filteredProducts = productList;
+
+        if (selectedCategory !== '') {
+            filteredProducts = productList.filter(product => product.category === selectedCategory);
+        }
+
+        const productTemp = filteredProducts.filter(product => {
             const idString = String(product.id); // Convert id to string
             return (
                 idString.includes(searchInput) ||
-                product.title.includes(searchInput) ||
-                product.description.includes(searchInput)
+                product.title.toLowerCase().includes(searchInput.toLowerCase()) ||
+                product.description.toLowerCase().includes(searchInput.toLowerCase())
             );
         });
 
@@ -24,7 +31,7 @@ export default function ProductList() {
 
         return (
             <tr>
-                <td colSpan={7}>no Items</td>
+                <td colSpan={7}>No Items</td>
             </tr>
         );
     };
@@ -34,6 +41,7 @@ export default function ProductList() {
             .then((response) => response.json())
             .then((response) => setProductList(response));
     };
+
     const getCategories = () => {
         fetch("https://fakestoreapi.com/products/categories")
             .then((response) => response.json())
@@ -46,15 +54,25 @@ export default function ProductList() {
     }, []);
 
     const handleSearch = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         const searchValue = document.querySelector('#Search').value;
-        setSearchInput(searchValue)
+        setSearchInput(searchValue);
+        setSelectedCategory(''); // Reset selected category when performing a search
+    };
+
+    const handleCategoryClick = (category) => {
+        setSearchInput(''); // Reset search input when selecting a category
+        setSelectedCategory(category);
     };
 
     const displayCategories = () => {
         if (Array.isArray(categories)) {
             return categories.map(category =>
-                <button className="btn btn-secondary">
+                <button
+                    key={category}
+                    className={`btn btn-secondary ${selectedCategory === category ? 'active' : ''}`}
+                    onClick={() => handleCategoryClick(category)}
+                >
                     {category}
                 </button>
             );
@@ -66,28 +84,29 @@ export default function ProductList() {
     return (
         <div className="container-fluid mx-auto w-75 my-3">
             <h2>Search :</h2>
-              <form>
-                  <div className="row g-3 align-items-center">
-                      <div className="col-auto">
-                      <label className="col-form-label">Search</label>
-                       </div>
-                      <div className="col-auto">
-                      <input type="text"  id="Search" className="form-control"/>
-                      </div>
-
-                  <div className="col-auto">
-                      <input type="submit" className="btn btn-primary" value="search" onClick={handleSearch}/>
-
-                  </div>
-                  </div>
-                  <div className="row g-3 align-items-center">
-                      <div className="btn-group">
-                          {displayCategories()}
-
-                      </div>
-                  </div>
-              </form>
-            <h1>produits :</h1>
+            <form>
+                <div className="row g-3 align-items-center">
+                    <div className="col-auto">
+                        <label className="col-form-label">Search</label>
+                    </div>
+                    <div className="col-auto">
+                        <input type="text" id="Search" className="form-control" />
+                    </div>
+                    <div className="col-auto">
+                        <input type="submit" className="btn btn-primary" value="Search" onClick={handleSearch} />
+                    </div>
+                </div>
+                <div className="row g-3 align-items-center">
+                    <h5>Categories :</h5>
+                    <div className="btn-group">
+                        <button className={`btn btn-secondary ${selectedCategory === '' ? 'active' : ''}`} onClick={() => handleCategoryClick('')}>
+                            All
+                        </button>
+                        {displayCategories()}
+                    </div>
+                </div>
+            </form>
+            <h1>Products :</h1>
             <table className="table">
                 <thead>
                 <tr>
