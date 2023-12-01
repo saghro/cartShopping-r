430e5c7c3c264079fb4e0154a0c794977e9d3a08
saghro <ayoubsaghro27@gmail.com>
2023-12-01 15:35:22 +0100

    used useRef

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Form from "./Basics/Form/Form";
 import FormRef from "./Basics/formRef/FormRef";
+import FormValidation from "./Basics/Formvalidation/FormValidation";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <FormRef/>
+      <FormValidation/>
   </React.StrictMode>
 );
 
