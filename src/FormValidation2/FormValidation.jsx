2c5b29b605069f757c735ff9b07ff449b8fa7cb4
saghro <ayoubsaghro27@gmail.com>
2023-12-04 16:55:36 +0100

    fn

--- src/FormValidation2/FormValidation.jsx
+++ src/FormValidation2/FormValidation.jsx
@@ -1,79 +1,169 @@
-import React, {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
-export default function  FormValidation(){
-    const name = useRef();
-    const email = useRef();
-    const message = useRef();
-    const acceptAllCondition = useRef();
-    const [formSent , setFormSent] = useState(false)
-    const [errors ,setErrors]=useState([])
+export default function FormValidation() {
+    const name = useRef()
+    const email = useRef()
+    const message = useRef()
+    const acceptAllConditions = useRef()
+    const [errors, setErrors] = useState({})
 
-   const validateForm = ()=>{
-       const nameValue = name.current.value;
-       const emailValue = email.current.value;
-       const messageValue = message.current.value;
-       const acceptAllConditionValue = acceptAllCondition.current.checked;
+    const [isFormSent, setIsFormSent] = useState(false)
+    const [isFormValid, setIsFormValid] = useState(false)
 
-       if(nameValue.trim() === ''){
-           alert('field name required')
-       }
-   }
-    const restForm = ()=>{
-        name.current.value=''
-        email.current.value=''
-        message.current.value=''
-        acceptAllCondition.current.checked=false
+    const validateRequiredElement = (ref) => {
+        if (ref.current.value.trim() === '') {
+            setErrors(prevState => {
+                    return {...prevState, ...{[ref.current.id]: 'field required'}}
+                }
+            )
+            setIsFormValid(false)
+        }
+    }
+    const validateForm = () => {
+        const emailValue = email.current.value
+        const acceptAllConditionsValue = acceptAllConditions.current.checked
+        let isFormValid = true
+
+        setErrors([])
+
+        validateRequiredElement(name)
+        validateRequiredElement(email)
+
+        if (!emailValue.match(/^\S+@\S+\.\S+$/)) {
+            setErrors(prevState => {
+                return {
+                    ...prevState,
+                    ...{email: 'Email incorrect'}
+                }
+            })
+            isFormValid = false
+        }
+
+        const minMessageLength = 200
+        const currentMessageLength = message.current.value.length
+
+        if (currentMessageLength < minMessageLength) {
+            setErrors(prevState => {
+                return {...prevState, ...{[message.current.id]: `Message should be greater than ${minMessageLength} (${currentMessageLength}/${minMessageLength})`}}
+            })
+            isFormValid = false
+        }
+        if (!acceptAllConditionsValue) {
+            setErrors(prevState => {
+                return {...prevState, ...{[acceptAllConditions.current.id]: `the checkbox must be checked`}}
+            })
+            isFormValid = false
+        }
+
+        setIsFormValid(isFormValid)
+        return isFormValid
     }
 
-    const submitForm = (e) => {
-        e.preventDefault();
+    const resetForm = () => {
+        name.current.value = ''
+        email.current.value = ''
+        message.current.value = ''
+        acceptAllConditions.current.checked = false
+    }
+    const handleChange = () => {
         validateForm()
-        const nameValue = name.current.value;
-        const emailValue = email.current.value;
-        const messageValue = message.current.value;
-        const acceptAllConditionValue = acceptAllCondition.current.checked;
-
-
-       // setFormSent(true)
-        //restForm()
-    };
-    return(
-        <div className={'container-fluid w-75 mx-auto my-5'}>
-            {formSent?  <div className="alert alert-success" role="alert">
-                <strong> form send successfully</strong>
-            </div>
-                : ''
+    }
+    const submitForm = (e) => {
+        e.preventDefault()
+        setIsFormSent(false)
+        if (validateForm()) {
+            setIsFormSent(true)
+            resetForm()
+        }
+    }
+    const getError = (fieldName) => {
+        return errors[fieldName]
+    }
+    const hasError = (fieldName) => {
+        return getError(fieldName) !== undefined
+    }
+    const displayError = (ref) => {
+        if (ref.current !== undefined) {
+            const fieldName = ref.current.id
+            if (hasError(fieldName)) {
+                ref.current.style.border = '1px solid red'
+                ref.current.style.backgroundColor = 'rgba(255,0,0,0.1)'
+                return <div className={'text-danger'}>{getError(fieldName)}</div>
             }
-
-            <form onSubmit={submitForm} >
+            ref.current.style.border = '1.5px solid green'
+            ref.current.style.backgroundColor = 'rgba(0,255,0,0.1)'
+        }
+    }
+    const displayErrors = () => {
+        return Object.entries(errors).map((error, key) => {
+            const [field, message] = error
+            return <li key={key}>{field} : {message}</li>
+        })
+    }
+    return <div className={'container-fluid w-75 mx-auto my-5'}>
+        {isFormSent ?
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-3">Message sent successfully !!</h1>
+                    <p className="lead">Thank you for your message</p>
+                    <hr className="my-2"/>
+                    <p>More info</p>
+                    <p className="lead">
+                        <a className="btn btn-primary btn-lg" href="" role="button">Return to contact page</a>
+                    </p>
+                </div>
+            </div>
+            : <form onSubmit={submitForm} onChange={handleChange}>
+                {Object.keys(errors).length > 0 ?
+                    <div className="alert alert-danger" role="alert">
+                        <strong>Errors</strong>
+                        <ul>
+                            {displayErrors()}
+                        </ul>
+                    </div>
+                    : ''
+                }
                 <h1>Contact form</h1>
                 <hr/>
+                {/*<-- Name input -->*/}
                 <div className="form-outline mb-4">
                     <label className="form-label" htmlFor="name">Name</label>
                     <input type="text" id="name" className="form-control" ref={name}/>
+                    {displayError(name)}
                 </div>
 
+                {/*<-- Email input -->*/}
                 <div className="form-outline mb-4">
                     <label className="form-label" htmlFor="email">Email address</label>
                     <input type="text" id="email" className="form-control" ref={email}/>
+                    {displayError(email)}
                 </div>
 
+                {/*<-- Message input -->*/}
                 <div className="form-outline mb-4">
                     <label className="form-label" htmlFor="message">Message</label>
                     <textarea className="form-control" id="message" rows="4" ref={message}></textarea>
+                    {displayError(message)}
                 </div>
 
+                {/*<-- Checkbox -->*/}
                 <div className="form-check mb-4">
                     <div className="d-flex">
-                        <input className="form-check-input me-2" type="checkbox" id="acceptAllConditions" ref={acceptAllCondition} />
+                        <input className="form-check-input me-2" type="checkbox" id="acceptAllConditions"
+                               ref={acceptAllConditions}/>
                         <label className="form-check-label" htmlFor="acceptAllConditions">
                             Accept all conditions
                         </label>
                     </div>
+
+                    {displayError(acceptAllConditions)}
                 </div>
 
-                <button type="submit" className="btn btn-primary w-100 mb-4">Submit</button>
+                {/*<-- Submit -->*/}
+                <button disabled={!isFormValid} type="submit" className="btn btn-primary w-100 mb-4">Submit</button>
             </form>
-        </div>
-    )
+        }
+
+
+    </div>
 }
