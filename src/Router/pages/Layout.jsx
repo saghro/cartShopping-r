a4153f93f1c7e89082612c9dcce17541e91e5a71
saghro <ayoubsaghro27@gmail.com>
2023-12-05 15:10:31 +0100

    add page

--- src/Router/pages/Layout.jsx
+++ src/Router/pages/Layout.jsx
@@ -4,18 +4,23 @@ import { Link } from 'react-router-dom';
 function Layout({ children }) {
     return (
         <>
-            <nav className='navbar-dark bg-primary nav justify-content-center'>
-                <ul className="nav">
-                    <li className="nav-item">
-                        <Link to='/' className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to='/blogs' className="nav-link">Blogs</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to='/contact' className="nav-link">Contact</Link>
-                    </li>
-                </ul>
+            <nav className='navbar navbar-expand-lg navbar-light bg-primary '>
+                <div className="container">
+                    <Link to='/' className="navbar-brand">Logo</Link>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to='/' className="nav-link">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/blogs' className="nav-link">Blogs</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/contact' className="nav-link">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
             <div className="container mt-4">
                 {children}
